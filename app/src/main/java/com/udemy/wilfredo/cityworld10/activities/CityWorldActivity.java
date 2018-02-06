0ca725979d2879ae68948fd5dfb72cbe205619f6
willimetro <wilfredo.ugalde@gmail.com>
2018-02-06 01:37:41 -0300

    Se agrega la primera version de la aplicación

---
+++ app/src/main/java/com/udemy/wilfredo/cityworld10/activities/CityWorldActivity.java
@@ -0,0 +1,15 @@
+package com.udemy.wilfredo.cityworld10.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.udemy.wilfredo.cityworld10.R;
+
+public class CityWorldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_world);
+    }
+}
---
+++ app/src/main/java/com/udemy/wilfredo/cityworld10/model/City.java
@@ -0,0 +1,11 @@
+package com.udemy.wilfredo.cityworld10.model;
+
+import io.realm.RealmObject;
+
+/**
+ * Clase que representa a una ciudad en la aplicación
+ * Created by wilfredo on 06-02-18.
+ */
+
+public class City extends RealmObject {
+}
